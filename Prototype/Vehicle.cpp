40a040138f2b163cc6fb821db2bdb2372513b3b5
Arkadiusz Pizon <62695014+awaw24@users.noreply.github.com>
2024-05-19 14:29:55 +0200

    Add files via upload

---
+++ Prototype/Vehicle.cpp
@@ -0,0 +1,11 @@
+#include "Vehicle.h"
+#include <iostream>
+Vehicle::Vehicle(): m_components{"Engine\nFuel tanks"},m_vehicleName{"Some default vehicle"}{}
+
+void Vehicle::setVehicleName(std::string vehicleName) {
+    this->m_vehicleName = vehicleName;
+}
+
+void Vehicle::addComponent(std::string component) {
+    this->m_components+=("\n" + component);
+}
